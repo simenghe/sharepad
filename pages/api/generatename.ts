343e39f394d8c8e85ee3b6309896abe295ef9080
Simeng He <28720612+simenghe@users.noreply.github.com>
2022-02-16 20:13:57 -0500

    Great progress

---
+++ pages/api/generatename.ts
@@ -0,0 +1,43 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { createClient } from "redis";
+import generateName from "../../utils/generateName";
+
+type Data = {
+  name: string;
+};
+const client = createClient();
+client.on("error", (err) => console.log("Redis Client Error", err));
+
+// Start the connection here.
+(async () => {
+  await client.connect();
+  console.log("REDIS client connected.");
+})();
+
+async function handleGet(req: NextApiRequest, res: NextApiResponse) {
+  const randomName: string = generateName();
+  return res.status(200).json({ name: randomName });
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    switch (req.method) {
+      case "GET": {
+        return handleGet(req, res);
+      }
+      case "POST": {
+        return res.status(200).json({ name: "JSON" });
+      }
+      default: {
+        return res.status(405);
+      }
+    }
+  } catch (err: unknown) {
+    console.error(err);
+    return res.status(500).json({ name: "error: " + err });
+  }
+}
---
+++ utils/generateName.ts
@@ -0,0 +1,17 @@
+import {
+  uniqueNamesGenerator,
+  Config,
+  adjectives,
+  colors,
+  animals,
+} from "unique-names-generator";
+
+const customConfig: Config = {
+  dictionaries: [adjectives, colors],
+  separator: "-",
+  length: 2,
+};
+
+export default function generateName(): string {
+  return uniqueNamesGenerator(customConfig); // big-donkey
+}
